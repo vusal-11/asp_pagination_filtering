3f8629e7bcd3ac00fd466d4a1ee6dd0f80d55342
Vusal Aliyev <vusal.aliyev7472@gmail.com>
2023-07-26 16:20:43 +0400

    Init

---
+++ ASP_EF_Pagination/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<StudentsDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("StudentsDbContext"),
+
+    setting =>
+    {
+        setting.CommandTimeout(30);
+        setting.MigrationsHistoryTable("EF_TABLE_MIGRATIONS");
+    }
+    
+    ));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Students}/{action=Index}/{id?}");
+
+app.Run();
